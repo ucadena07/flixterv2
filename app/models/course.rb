0cfa1acf3eedd9230c397f6a0c27001b109dc413
Ulises Cadena <ucadena07@gmail.com>
2020-03-03 07:22:39 -0600

    Carrierwave

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,10 @@
 class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :sections
   has_many :lessons
+  has_many :photos
+  has_many :images
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
