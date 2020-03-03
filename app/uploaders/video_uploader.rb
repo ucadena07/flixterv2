66a1a487aa5f4c8463909de9b0bbf0a35399463c
Ulises Cadena <ucadena07@gmail.com>
2020-03-03 10:01:59 -0600

    Video Uploading

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,11 +1,12 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-   storage :aws
+  #storage :file
+  # storage :fog
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -30,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fit: [800, 350]
+  #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
