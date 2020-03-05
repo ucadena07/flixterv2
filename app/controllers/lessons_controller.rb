e2186c750ea98dd8cc15c0bfb8f200b55056f07a
Ulises Cadena <ucadena07@gmail.com>
2020-03-04 19:30:57 -0600

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :required_authorized_user_for_current_lesson
+
   def show
   end
 
   private
 
-  helper_method :current_lesson 
+  def required_authorized_user_for_current_lesson
+    if not current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled in this course.'
+    end
+  end
 
+  helper_method :current_lesson 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
