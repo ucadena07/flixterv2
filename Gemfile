c101b59492ee11a44d914c32cdd66ed38e71ac34
Ulises Cadena <ucadena07@gmail.com>
2020-03-05 07:41:39 -0600

    Ordering Lessons

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ gem "mini_magick"
 gem 'carrierwave-aws'
 
 gem 'stripe'
+
+gem 'ranked-model'
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,9 @@ class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
 
+  include RankedModel
+  ranks :row_order, with_same: :section_id
+
   validates :title, presence: true
   validates :subtitle, presence: true
 end
